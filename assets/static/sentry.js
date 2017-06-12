d779448aa505951f8d9a0ddf099fca6acd753b77
Łukasz Mierzwa <l.mierzwa@gmail.com>
2017-06-12 16:56:02 -0700

    Remove useless replace

--- assets/static/sentry.js
+++ assets/static/sentry.js
@@ -4,7 +4,7 @@
 
     // init sentry client if sentry dsn is set
     if ($("body").data("raven-dsn")) {
-        var dsn = $("body").data("raven-dsn").replace("\/", "/");
+        var dsn = $("body").data("raven-dsn");
         // raven itself can fail if invalid DSN is passed
         try {
             Raven.config(dsn, {
