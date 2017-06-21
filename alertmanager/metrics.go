ccbac56cd77f99c08da90033e2cc40a8c204473a
Łukasz Mierzwa <l.mierzwa@gmail.com>
2017-06-20 20:47:14 -0700

    Add upstream abstraction to alertmanager package

---
+++ alertmanager/metrics.go
@@ -0,0 +1,33 @@
+package alertmanager
+
+import "github.com/prometheus/client_golang/prometheus"
+
+var (
+	metricAlerts = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "unsee_collected_alerts",
+			Help: "Total number of alerts collected from Alertmanager API",
+		},
+		[]string{"alertmanager", "state"},
+	)
+	metricAlertGroups = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "unsee_collected_groups",
+			Help: "Total number of alert groups collected from Alertmanager API",
+		},
+		[]string{"alertmanager"},
+	)
+	metricAlertmanagerErrors = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "unsee_alertmanager_errors_total",
+			Help: "Total number of errors encounter when requesting data from Alertmanager API",
+		},
+		[]string{"alertmanager", "endpoint"},
+	)
+)
+
+func init() {
+	prometheus.MustRegister(metricAlerts)
+	prometheus.MustRegister(metricAlertGroups)
+	prometheus.MustRegister(metricAlertmanagerErrors)
+}
