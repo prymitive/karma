07a272e49327bb7d745d59aca680bf0e6190599d
Łukasz Mierzwa <l.mierzwa@gmail.com>
2017-07-19 22:05:22 -0700

    Fix watchdog tests

--- assets/static/watchdog.test.js
+++ assets/static/watchdog.test.js
@@ -1,9 +1,16 @@
 const watchdog = require("./watchdog");
+const moment = require("moment");
 
-test("watchdog init", () => {
+test("watchdog init()", () => {
     watchdog.init();
 });
 
-test("watchdog getTs without pong", () => {
-    expect(watchdog.getTs).toBe(undefined);
+test("watchdog getLastUpdate() without pong", () => {
+    expect(watchdog.getLastUpdate()).toBe(0);
+});
+
+test("watchdog getLastUpdate() with pong", () => {
+    var ts = moment();
+    watchdog.pong(ts);
+    expect(watchdog.getLastUpdate()).toBe(ts.utc().unix());
 });
