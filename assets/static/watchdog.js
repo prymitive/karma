1474af8f07149ddc163d5e681a767f8fdc3dcb42
Łukasz Mierzwa <l.mierzwa@gmail.com>
2017-07-19 21:22:09 -0700

    Rewrite watchdog using CommonJS

--- assets/static/watchdog.js
+++ assets/static/watchdog.js
@@ -1,74 +1,63 @@
-/* globals moment */    // moment.js
+var selectors = {
+    countdown: "#reload-counter"
+};
 
-/* globals Config, Counter, Templates */
+var lastTs = 0;
+var maxLag;
 
-/* exported Watchdog */
-var Watchdog = (function() {
+var inCountdown = false;
+var fatalCountdown = 60;
+var fatalReloadTimer = false;
+var fatalCounterTimer = false;
 
-    var selectors = {
-        countdown: "#reload-counter"
-    };
+function timerTick() {
+    if (lastTs === 0) return;
 
-    var lastTs = 0;
-    var maxLag;
+    // don't raise an error if autorefresh is disabled
+    if (!Config.GetOption("autorefresh").Get()) return;
 
-    var inCountdown = false;
-    var fatalCountdown = 60;
-    var fatalReloadTimer = false;
-    var fatalCounterTimer = false;
-
-    var timerTick = function() {
-        if (lastTs === 0) return;
-
-        // don't raise an error if autorefresh is disabled
-        if (!Config.GetOption("autorefresh").Get()) return;
-
-        var now = moment().utc().unix();
-        if (now - lastTs > maxLag) {
-            $("#errors").html(Templates.Render("fatalError", {
-                lastTs: lastTs,
-                secondsLeft: fatalCountdown
-            })).show();
-            Counter.Unknown();
-            if (!inCountdown) {
-                fatalCountdown = 60;
-                fatalReloadTimer = setTimeout(function() {
-                    location.reload();
-                }, 60 * 1000);
-                fatalCounterTimer = setInterval(function() {
-                    $(selectors.countdown).text(--fatalCountdown);
-                }, 1000);
-                inCountdown = true;
-            }
-        } else {
-            inCountdown = false;
-            if (fatalReloadTimer) clearTimeout(fatalReloadTimer);
-            if (fatalCounterTimer) clearTimeout(fatalCounterTimer);
+    var now = moment().utc().unix();
+    if (now - lastTs > maxLag) {
+        $("#errors").html(Templates.Render("fatalError", {
+            lastTs: lastTs,
+            secondsLeft: fatalCountdown
+        })).show();
+        Counter.Unknown();
+        if (!inCountdown) {
+            fatalCountdown = 60;
+            fatalReloadTimer = setTimeout(function() {
+                location.reload();
+            }, 60 * 1000);
+            fatalCounterTimer = setInterval(function() {
+                $(selectors.countdown).text(--fatalCountdown);
+            }, 1000);
+            inCountdown = true;
         }
-    };
+    } else {
+        inCountdown = false;
+        if (fatalReloadTimer) clearTimeout(fatalReloadTimer);
+        if (fatalCounterTimer) clearTimeout(fatalCounterTimer);
+    }
+}
 
-    var init = function(interval, tolerance) {
-        maxLag = tolerance;
-        setInterval(timerTick, interval * 1000);
-    };
+function init(interval, tolerance) {
+    maxLag = tolerance;
+    setInterval(timerTick, interval * 1000);
+}
 
-    var updateTs = function(ts) {
-        lastTs = ts.utc().unix();
-    };
+function pong(ts) {
+    lastTs = ts.utc().unix();
+}
 
-    var getTs = function() {
-        return lastTs;
-    };
+function getLastUpdate() {
+    return lastTs;
+}
 
-    var getFatal = function() {
-        return inCountdown;
-    };
+function isFatal() {
+    return inCountdown;
+}
 
-    return {
-        Init: init,
-        Pong: updateTs,
-        GetLastUpdate: getTs,
-        IsFatal: getFatal
-    };
-
-}());
+exports.init = init;
+exports.pong = pong;
+exports.getLastUpdate = getLastUpdate;
+exports.isFatal = isFatal;
