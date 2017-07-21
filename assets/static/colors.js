f22959f5d8a0dc3c70bbf8d9c783013cf6103844
Łukasz Mierzwa <l.mierzwa@gmail.com>
2017-07-20 22:16:29 -0700

    Rewrite colors.js as CommonJS, add tests

--- assets/static/colors.js
+++ assets/static/colors.js
@@ -1,61 +1,62 @@
-/* exported Colors */
-var Colors = (function() {
+const $ = require("jquery");
 
-    var colors;
+var colors = {},
+    staticColorLabels = [];
 
-    var specialLabels = {
-        "@state: unprocessed": "label-default",
-        "@state: active": "label-danger",
-        "@state: suppressed": "label-success",
-    };
+var specialLabels = {
+    "@state: unprocessed": "label-default",
+    "@state: active": "label-danger",
+    "@state: suppressed": "label-success",
+};
 
-    var update = function(colorData) {
-        colors = colorData;
-    };
+function init(staticColors) {
+    staticColorLabels = staticColors;
+}
 
-    var merge = function(colorData) {
-        $.extend(colors, colorData);
-    };
+function getStaticLabels() {
+    return staticColorLabels;
+}
 
-    var getClass = function(key, value) {
-        var label = key + ": " + value;
-        if (key == "alertname") {
-            return "label-primary";  // special case for alertname label, which is the name of alert
-        } else if (specialLabels[label] !== undefined) {
-            return specialLabels[label];
-        } else if (Colors.IsStaticLabel(key)) {
-            return "label-info";
-        } else {
-            return "label-warning";
-        }
-    };
+function isStaticLabel(key) {
+    return ($.inArray(key, getStaticLabels()) >= 0);
+}
 
-    var getStyle = function(key, value) {
-        // get color data, returned as css style string
-        var style = "";
-        if (colors[key] !== undefined && colors[key][value] !== undefined) {
-            var c = colors[key][value];
-            style = "background-color: rgba(" + [ c.background.red, c.background.green, c.background.blue, c.background.alpha ].join(", ") + "); ";
-            style += "color: rgba(" + [ c.font.red, c.font.green, c.font.blue, c.font.alpha ].join(", ") + "); ";
-        }
-        return style;
-    };
+function update(colorData) {
+    colors = colorData;
+}
 
-    var getStaticLabels = function() {
-        return $("#alerts").data("static-color-labels").split(" ");
-    };
+function merge(colorData) {
+    $.extend(colors, colorData);
+}
 
-    var isStaticLabel = function(key) {
-        return ($.inArray(key, Colors.GetStaticLabels()) >= 0);
-    };
+function getClass(key, value) {
+    var label = key + ": " + value;
+    if (key === "alertname") {
+        return "label-primary";  // special case for alertname label, which is the name of alert
+    } else if (specialLabels[label] !== undefined) {
+        return specialLabels[label];
+    } else if (isStaticLabel(key)) {
+        return "label-info";
+    } else {
+        return "label-warning";
+    }
+}
 
-    return {
-        Update: update,
-        Merge: merge,
-        Get: getStyle,
-        GetClass: getClass,
-        GetStaticLabels: getStaticLabels,
-        IsStaticLabel: isStaticLabel
-    };
+function getStyle(key, value) {
+    // get color data, returned as css style string
+    var style = "";
+    if (colors[key] !== undefined && colors[key][value] !== undefined) {
+        var c = colors[key][value];
+        style = "background-color: rgba(" + [ c.background.red, c.background.green, c.background.blue, c.background.alpha ].join(", ") + "); ";
+        style += "color: rgba(" + [ c.font.red, c.font.green, c.font.blue, c.font.alpha ].join(", ") + "); ";
+    }
+    return style;
+}
 
-})();
+exports.init = init;
+exports.update = update;
+exports.merge = merge;
+exports.getStyle = getStyle;
+exports.getClass = getClass;
+exports.getStaticLabels = getStaticLabels;
+exports.isStaticLabel = isStaticLabel;
