7301f407787714f34ccf0147a9aef68ed49cd240
Łukasz Mierzwa <l.mierzwa@gmail.com>
2017-07-21 22:04:51 -0700

    Rewrite sentry.js as CommonJS, add basic test

--- assets/static/sentry.js
+++ assets/static/sentry.js
@@ -1,19 +1,16 @@
-/* globals Raven */     // raven.js
+const Raven = require("raven-js");
+const $ = require("jquery");
 
-(function() {
-
-    // init sentry client if sentry dsn is set
-    if ($("body").data("raven-dsn")) {
-        var dsn = $("body").data("raven-dsn");
-        // raven itself can fail if invalid DSN is passed
-        try {
-            Raven.config(dsn, {
-                release: $("body").data("unsee-version")
-            }).install();
-        } catch (error) {
-            var msg = "Sentry error: " + error.message;
-            $("#raven-error").text(msg).removeClass("hidden");
-        }
+// init sentry client if sentry dsn is set
+if ($("body").data("raven-dsn")) {
+    var dsn = $("body").data("raven-dsn");
+    // raven itself can fail if invalid DSN is passed
+    try {
+        Raven.config(dsn, {
+            release: $("body").data("unsee-version")
+        }).install();
+    } catch (error) {
+        var msg = "Sentry error: " + error.message;
+        $("#raven-error").text(msg).removeClass("hidden");
     }
-
-})();
+}
