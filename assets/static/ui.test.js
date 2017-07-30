558ace10fc9867ee3d2923315c3d1b9494d0bf92
Łukasz Mierzwa <l.mierzwa@gmail.com>
2017-07-30 11:21:33 -0700

    More coverage in the ui.js module tests

--- assets/static/ui.test.js
+++ assets/static/ui.test.js
@@ -1,19 +1,28 @@
-const $ = require("jquery");
+const $ = window.$ = window.jQuery = require("jquery");
 
 test("ui init()", () => {
-    window.jQuery = require("jquery");
     const ui = require("./ui");
     ui.init();
 });
 
 test("ui setupAlertGroupUI()", () => {
-    window.jQuery = require("jquery");
     const ui = require("./ui");
     ui.setupAlertGroupUI($("<div></div>"));
 });
 
 test("ui setupGroupTooltips()", () => {
-    window.jQuery = require("jquery");
+    document.body.innerHTML =
+        "<div id='groupTest'>" +
+        "  <span id='foo' title='foo' data-toggle='tooltip'></span>" +
+        "  <span id='bar' data-ts-title='bar' data-toggle='tooltip'></span>" +
+        "</div>";
+    require("bootstrap/js/tooltip.js");
     const ui = require("./ui");
-    ui.setupGroupTooltips($("<div></div>"));
+    ui.setupGroupTooltips($("#groupTest"));
+    // check if bootstrap tooltip was applied, it will empty tooltip attr if set
+    // and save it under data-original-title
+    expect($("#foo").attr("title")).toBe("");
+    expect($("#foo").data("original-title")).toBe("foo");
+    expect($("#bar").attr("title")).toBe("");
+    expect($("#bar").data("original-title")).toBe("");
 });
