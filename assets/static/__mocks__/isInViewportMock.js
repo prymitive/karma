90646a3727f48999272374ec6116595eb79f314c
Łukasz Mierzwa <l.mierzwa@gmail.com>
2017-08-02 20:11:32 -0700

    Fix is-in-viewport mock to return true

--- assets/static/__mocks__/isInViewportMock.js
+++ assets/static/__mocks__/isInViewportMock.js
@@ -1,11 +1,11 @@
 const $ = require("jquery");
 
-function isInViewport() {}
+// always returns true, indicating that every tested element is in viewport
+function isInViewport() {
+    return true;
+}
 $.extend($.expr[":"], {
     "in-viewport": $.expr.createPseudo
         ? $.expr.createPseudo(argsString => currElement => isInViewport(currElement, argsString))
         : (currObj, index, meta) => isInViewport(currObj, meta)
 })
-
-function run() {}
-$.fn.run = run
