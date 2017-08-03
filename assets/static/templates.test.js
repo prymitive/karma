ae5f0e39ae817f0878427a1fe4520805ca34f3b5
Łukasz Mierzwa <l.mierzwa@gmail.com>
2017-08-02 20:57:24 -0700

    remove dead code from templates.test.js

--- assets/static/templates.test.js
+++ assets/static/templates.test.js
@@ -1,12 +1,7 @@
-const $ = require("jquery");
 const templates = require("./templates");
 const templatesMock = require("./__mocks__/templatesMock");
 
 test("templates init()", () => {
-    var elems = [];
-    $.each(templates.getConfig(), function(name, selector) {
-        elems.push("<div class='" + name + "' id='" + selector.slice(1) + "'></div>");
-    });
     document.body.innerHTML = templatesMock.loadTemplates();
     templates.init();
 });
