55c722773af86be9f7a590a2f8d8e89232a5faf8
Łukasz Mierzwa <l.mierzwa@gmail.com>
2017-08-05 22:09:57 -0700

    Resume auto-refresh on backspace
    Right now if user starts typing & then starts pressing backspace we pause but never resume, because stopping pressing backspace is ignored when resuming.

--- assets/static/filters.js
+++ assets/static/filters.js
@@ -118,10 +118,12 @@ function init() {
     // stop when user is typing in the filter bar
     $(".bootstrap-tagsinput").typing({
         start: function(event) {
+            // ignore backspace & enter
             if (event.keyCode != 8 && event.keyCode != 13) unsee.pause();
         },
         stop: function(event) {
-            if (event.keyCode != 8 && event.keyCode != 13) unsee.resume();
+            // ignore enter
+            if (event.keyCode != 13) unsee.resume();
         },
         delay: 1000
     });
