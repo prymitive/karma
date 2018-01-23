338a4c38a6d6c9c10066e2865b2533d6fb4ff478
Łukasz Mierzwa <l.mierzwa@gmail.com>
2018-01-22 23:13:37 -0800

    Drop duplicated function
    There's identical method on the Alertmanager instance that's being used

--- internal/alertmanager/version.go
+++ internal/alertmanager/version.go
@@ -1,15 +1,5 @@
 package alertmanager
 
-import (
-	"crypto/tls"
-	"encoding/json"
-	"time"
-
-	"github.com/cloudflare/unsee/internal/transport"
-
-	log "github.com/sirupsen/logrus"
-)
-
 // AlertmanagerVersion is what api/v1/status returns, we only use it to check
 // version, so we skip all other keys (except for status)
 type alertmanagerVersion struct {
@@ -19,43 +9,4 @@ type alertmanagerVersion struct {
 			Version string `json:"version"`
 		} `json:"versionInfo"`
 	} `json:"data"`
-}
-
-// GetVersion returns version information of the remote Alertmanager endpoint
-func GetVersion(uri string, timeout time.Duration, tlsConfig *tls.Config) string {
-	// if everything fails assume Alertmanager is at latest possible version
-	defaultVersion := "999.0.0"
-
-	url, err := transport.JoinURL(uri, "api/v1/status")
-	if err != nil {
-		log.Errorf("Failed to join url '%s' and path 'api/v1/status': %s", uri, err.Error())
-		return defaultVersion
-	}
-	ver := alertmanagerVersion{}
-
-	t, err := transport.NewTransport(uri, timeout, tlsConfig)
-	if err != nil {
-		log.Errorf("Unable to get the version information from %s", url)
-		return defaultVersion
-	}
-
-	source, err := t.Read(url)
-	err = json.NewDecoder(source).Decode(&ver)
-	if err != nil {
-		log.Errorf("%s request failed: %s", url, err.Error())
-		return defaultVersion
-	}
-
-	if ver.Status != "success" {
-		log.Errorf("Request to %s returned status %s", url, ver.Status)
-		return defaultVersion
-	}
-
-	if ver.Data.VersionInfo.Version == "" {
-		log.Error("No version information in Alertmanager API")
-		return defaultVersion
-	}
-
-	log.Infof("Remote Alertmanager version: %s", ver.Data.VersionInfo.Version)
-	return ver.Data.VersionInfo.Version
 }
