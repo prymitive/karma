c80bde05b0920675cc175fcced42ba958c8bbf90
Łukasz Mierzwa <l.mierzwa@gmail.com>
2018-07-19 22:35:55 +0200

    chore(ui): drop why-did-you-update
    Not that useful with mobx

--- ui/src/index.js
+++ ui/src/index.js
@@ -51,14 +51,6 @@ if (settings !== null) {
   }
 }
 
-// enable console warnings, but only for dev
-if (process.env.NODE_ENV === "development") {
-  const { whyDidYouUpdate } = require("why-did-you-update");
-  whyDidYouUpdate(React, {
-    exclude: [/^Linkify$/, /^CSSTransition$/, /^inject-/, /^InnerReference$/]
-  });
-}
-
 // global timer for updating timestamps to human readable offsets
 // this needs to be run before any <Moment/> instance
 // https://www.npmjs.com/package/react-moment#pooled-timer
