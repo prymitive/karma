af65a988b50a114b3d23a96de908d45f1a9752a0
Łukasz Mierzwa <l.mierzwa@gmail.com>
2018-08-25 18:27:49 +0100

    feat(tests): add silenceFormStore tests

---
+++ ui/src/__mocks__/Alerts.js
@@ -0,0 +1,53 @@
+const MockAnnotation = (name, value, visible, isLink) => ({
+  name: name,
+  value: value,
+  visible: visible,
+  isLink: isLink
+});
+
+const MockAlert = (annotations, labels, state) => ({
+  annotations: annotations,
+  labels: labels,
+  startsAt: "2018-08-14T17:36:40.017867056Z",
+  endsAt: "0001-01-01T00:00:00Z",
+  state: state,
+  alertmanager: [
+    {
+      name: "default",
+      uri: "file:///mock",
+      state: "active",
+      startsAt: "2018-08-14T17:36:40.017867056Z",
+      endsAt: "0001-01-01T00:00:00Z",
+      source: "localhost/prometheus",
+      silencedBy: []
+    }
+  ],
+  receiver: "by-name"
+});
+
+const MockAlertGroup = (
+  rootLabels,
+  alerts,
+  sharedAnnotations,
+  sharedLabels
+) => ({
+  receiver: "by-name",
+  labels: rootLabels,
+  alerts: alerts,
+  id: "099c5ca6d1c92f615b13056b935d0c8dee70f18c",
+  hash: "53a4bb3d7e916450b3bda550976f9578db5b2ad3",
+  alertmanagerCount: {
+    default: 1
+  },
+  stateCount: {
+    active: 1,
+    suppressed: 0,
+    unprocessed: 0
+  },
+  shared: {
+    annotations: sharedAnnotations,
+    labels: sharedLabels
+  }
+});
+
+export { MockAnnotation, MockAlert, MockAlertGroup };
