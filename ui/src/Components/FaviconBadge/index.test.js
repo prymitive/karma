6952650eeb6d9ebaa369df68f432b3ff8b7d7485
Łukasz Mierzwa <l.mierzwa@gmail.com>
2018-08-30 22:15:42 +0100

    feat(tests): add test coverage for FaviconBadge

---
+++ ui/src/Components/FaviconBadge/index.test.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+import { mount } from "enzyme";
+
+import { AlertStore } from "Stores/AlertStore";
+import { FaviconBadge } from ".";
+
+let alertStore;
+
+beforeEach(() => {
+  alertStore = new AlertStore([]);
+});
+
+const MountedFaviconBadge = () => {
+  return mount(<FaviconBadge alertStore={alertStore} />);
+};
+
+describe("<FaviconBadge />", () => {
+  it("creates Favico instance on mount", () => {
+    const tree = MountedFaviconBadge();
+    const instance = tree.instance();
+    expect(instance.favicon).toBeInstanceOf(Object);
+  });
+
+  it("updateBadge is called when alertStore.info.totalAlerts changes", () => {
+    const tree = MountedFaviconBadge();
+    const instance = tree.instance();
+    const updateSpy = jest.spyOn(instance, "updateBadge");
+    alertStore.info.totalAlerts = 99;
+    expect(updateSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it("updateBadge is called when alertStore.status.error changes", () => {
+    const tree = MountedFaviconBadge();
+    const instance = tree.instance();
+    const updateSpy = jest.spyOn(instance, "updateBadge");
+    alertStore.status.error = "foo";
+    expect(updateSpy).toHaveBeenCalledTimes(1);
+  });
+});
