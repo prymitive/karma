a57e3c7c5b00c76204a5a9361b034f70db86f90f
Łukasz Mierzwa <l.mierzwa@gmail.com>
2019-03-07 15:58:27 +0000

    fix(backend): fix the syntax of tools.go
    Comment was incorrectly formatted, resulting in tools.go being ignored.
    Fix the comment and run 'go mod tidy' to fix Go module files

--- tools.go
+++ tools.go
@@ -1,13 +1,14 @@
 // +build tools
 
-# https://github.com/golang/go/issues/25922#issuecomment-412992431
-# this file is to track dev dependencies for go modules that are needed
-# to build this project, but are only CLI tools and don't get imported
+// https://github.com/golang/go/issues/25922#issuecomment-412992431
+// this file is to track dev dependencies for go modules that are needed
+// to build this project, but are only CLI tools and don't get imported
 
 package tools
 
 import (
-        _ "github.com/elazarl/go-bindata-assetfs"
-        _ "github.com/terinjokes/bakelite"
-        _ "github.com/golangci/golangci-lint/pkg/commands"
+	_ "github.com/elazarl/go-bindata-assetfs"
+	_ "github.com/go-bindata/go-bindata"
+	_ "github.com/golangci/golangci-lint/pkg/commands"
+	_ "github.com/terinjokes/bakelite"
 )
