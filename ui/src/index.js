1b5d856a2136bfa30a0e858e25ab390571785ef8
Łukasz Mierzwa <l.mierzwa@gmail.com>
2019-04-23 21:29:08 +0100

    refactor(ui): use react-app-polyfill instead of @babel/polyfill
    CRA 3.0 depracates babel/polyfill, react-app-polyfill is the new recommended pollyfill package.
    Remove IE from the list of supported browsers and drop polyfills for it since doesn't work anyway - Proxy support in ES is required for mobx 5.x and that cannot be polyfilled.

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,6 +1,6 @@
 // MUST be first thing we import
-// https://babeljs.io/docs/en/babel-polyfill#usage-in-node-browserify-webpack
-import "@babel/polyfill";
+// https://github.com/facebook/create-react-app/blob/master/packages/react-app-polyfill/README.md
+import "react-app-polyfill/stable";
 
 // fetch() polyfill for IE and some mobile browsers
 import "whatwg-fetch";
