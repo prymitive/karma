8117000cb82e2cdd621cccf00de349447a488e04
Łukasz Mierzwa <l.mierzwa@gmail.com>
2019-06-07 21:15:46 +0100

    fix(backend): add a workaround for Go modules issue
    go.mod keeps getting updated on build but go mod tidy reverts these changes
    
    See https://github.com/golang/go/issues/31248#issuecomment-500019917

--- tools.go
+++ tools.go
@@ -9,6 +9,7 @@ package tools
 import (
 	_ "github.com/elazarl/go-bindata-assetfs"
 	_ "github.com/go-bindata/go-bindata"
+	_ "github.com/go-logfmt/logfmt" // workaround for https://github.com/golang/go/issues/31248
 	_ "github.com/golangci/golangci-lint/pkg/commands"
 	_ "github.com/terinjokes/bakelite"
 )
