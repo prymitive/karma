48b906b887e6f44cf446d77a2bf7a3bc2715a2de
Łukasz Mierzwa <l.mierzwa@gmail.com>
2019-10-08 21:24:41 +0100

    chore(ui): use mug-hot icon for empty grid

--- ui/src/Components/Grid/EmptyGrid/index.js
+++ ui/src/Components/Grid/EmptyGrid/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCheckCircle } from "@fortawesome/free-solid-svg-icons/faCheckCircle";
+import { faMugHot } from "@fortawesome/free-solid-svg-icons/faMugHot";
 
 import "./index.scss";
 
 const EmptyGrid = () => (
   <h1 className="display-1 text-secondary screen-center">
-    <FontAwesomeIcon icon={faCheckCircle} style={{ fontSize: "14rem" }} />
+    <FontAwesomeIcon icon={faMugHot} style={{ fontSize: "14rem" }} />
   </h1>
 );
 
