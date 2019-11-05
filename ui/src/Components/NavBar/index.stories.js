0bec6135cb8130b4c6978c5264e24d9d46347576
Łukasz Mierzwa <l.mierzwa@gmail.com>
2019-11-05 09:12:54 +0000

    fix(storybook): remove unused modal classes from navbar

--- ui/src/Components/NavBar/index.stories.js
+++ ui/src/Components/NavBar/index.stories.js
@@ -20,56 +20,40 @@ const NewFilter = (raw, name, matcher, value, applied, isValid, hits) => {
   return filter;
 };
 
-storiesOf("NavBar", module)
-  .addDecorator(storyFn => (
-    <div>
-      <div className="modal-dialog modal-lg" role="document">
-        <div className="modal-content">{storyFn()}</div>
-      </div>
-    </div>
-  ))
-  .add("NavBar", () => {
-    const alertStore = new AlertStore([]);
-    const settingsStore = new Settings();
-    const silenceFormStore = new SilenceFormStore();
+storiesOf("NavBar", module).add("NavBar", () => {
+  const alertStore = new AlertStore([]);
+  const settingsStore = new Settings();
+  const silenceFormStore = new SilenceFormStore();
 
-    alertStore.info.totalAlerts = 197;
-    alertStore.data.colors = {
-      cluster: {
-        staging: {
-          brightness: 205,
-          background: { red: 246, green: 176, blue: 247, alpha: 255 }
-        }
-      },
-      region: {
-        AF: {
-          brightness: 111,
-          background: { red: 115, green: 101, blue: 152, alpha: 255 }
-        }
+  alertStore.info.totalAlerts = 197;
+  alertStore.data.colors = {
+    cluster: {
+      staging: {
+        brightness: 205,
+        background: { red: 246, green: 176, blue: 247, alpha: 255 }
       }
-    };
+    },
+    region: {
+      AF: {
+        brightness: 111,
+        background: { red: 115, green: 101, blue: 152, alpha: 255 }
+      }
+    }
+  };
 
-    alertStore.filters.values = [
-      NewFilter("cluster=staging", "cluster", "=", "staging", true, true, 15),
-      NewFilter("region=AF", "region", "=", "AF", true, true, 180),
-      NewFilter(
-        "instance!=server1",
-        "instance",
-        "!=",
-        "server1",
-        false,
-        true,
-        0
-      ),
-      NewFilter("server!!!=", "", "", "", true, false, 0),
-      NewFilter("foo", "", "", "", true, true, 2)
-    ];
+  alertStore.filters.values = [
+    NewFilter("cluster=staging", "cluster", "=", "staging", true, true, 15),
+    NewFilter("region=AF", "region", "=", "AF", true, true, 180),
+    NewFilter("instance!=server1", "instance", "!=", "server1", false, true, 0),
+    NewFilter("server!!!=", "", "", "", true, false, 0),
+    NewFilter("foo", "", "", "", true, true, 2)
+  ];
 
-    return (
-      <NavBar
-        alertStore={alertStore}
-        settingsStore={settingsStore}
-        silenceFormStore={silenceFormStore}
-      />
-    );
-  });
+  return (
+    <NavBar
+      alertStore={alertStore}
+      settingsStore={settingsStore}
+      silenceFormStore={silenceFormStore}
+    />
+  );
+});
