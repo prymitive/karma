09f31e172b2ee3f007ef88a533fd76de38038d50
Łukasz Mierzwa <l.mierzwa@gmail.com>
2019-11-14 13:20:45 +0000

    fix(ui): follow redirects when fetching alerts from the UI

--- ui/src/Common/Fetch.js
+++ ui/src/Common/Fetch.js
@@ -1,6 +1,9 @@
 import merge from "lodash.merge";
 
 const FetchWithCredentials = async (uri, options) =>
-  await fetch(uri, merge({}, { credentials: "include" }, options));
+  await fetch(
+    uri,
+    merge({}, { credentials: "include", redirect: "follow" }, options)
+  );
 
 export { FetchWithCredentials };
