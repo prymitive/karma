ec3fdd47ecb95e26e0cfd0ccdf0cae74de92b775
Łukasz Mierzwa <l.mierzwa@gmail.com>
2019-12-12 22:34:41 +0000

    fix(ui): add tests for CenteredMessage

---
+++ ui/src/Components/CenteredMessage/index.test.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+import { shallow } from "enzyme";
+
+import toDiffableHtml from "diffable-html";
+
+import { CenteredMessage } from ".";
+
+describe("<CenteredMessage />", () => {
+  const Message = () => <div>Foo</div>;
+
+  it("matches snapshot", () => {
+    const tree = shallow(
+      <CenteredMessage>
+        <Message />
+      </CenteredMessage>
+    );
+    expect(toDiffableHtml(tree.html())).toMatchSnapshot();
+  });
+
+  it("uses 'display-1 text-placeholder' className by default", () => {
+    const tree = shallow(
+      <CenteredMessage>
+        <Message />
+      </CenteredMessage>
+    );
+    expect(toDiffableHtml(tree.html())).toMatch(/display-1 text-placeholder/);
+  });
+
+  it("uses custom className if passed", () => {
+    const tree = shallow(
+      <CenteredMessage className="bar-class">
+        <Message />
+      </CenteredMessage>
+    );
+    expect(toDiffableHtml(tree.html())).toMatch(/bar-class/);
+    expect(toDiffableHtml(tree.html())).not.toMatch(
+      /display-1 text-placeholder/
+    );
+  });
+});
