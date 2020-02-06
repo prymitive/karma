62b7d85f7080465f3fc34fc94beb093124abbecf
Łukasz Mierzwa <l.mierzwa@gmail.com>
2020-02-06 15:08:17 +0000

    fix(tests): remove v0.61 mapper
    That version had problems, remove it

--- internal/alertmanager/mapper.go
+++ internal/alertmanager/mapper.go
@@ -6,7 +6,6 @@ import (
 	v017 "github.com/prymitive/karma/internal/mapper/v017"
 	v04 "github.com/prymitive/karma/internal/mapper/v04"
 	v05 "github.com/prymitive/karma/internal/mapper/v05"
-	v061 "github.com/prymitive/karma/internal/mapper/v061"
 	v062 "github.com/prymitive/karma/internal/mapper/v062"
 )
 
@@ -14,7 +13,6 @@ import (
 func init() {
 	mapper.RegisterAlertMapper(v04.AlertMapper{})
 	mapper.RegisterAlertMapper(v05.AlertMapper{})
-	mapper.RegisterAlertMapper(v061.AlertMapper{})
 	mapper.RegisterAlertMapper(v062.AlertMapper{})
 	mapper.RegisterSilenceMapper(v04.SilenceMapper{})
 	mapper.RegisterSilenceMapper(v05.SilenceMapper{})
