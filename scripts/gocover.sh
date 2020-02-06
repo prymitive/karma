66132b9c18bae6a3b47ccbf9b50acf50383761c8
Łukasz Mierzwa <l.mierzwa@gmail.com>
2020-02-06 12:58:16 +0000

    fix(tests): more test coverage for mapper

--- scripts/gocover.sh
+++ scripts/gocover.sh
@@ -4,10 +4,10 @@ set -e
 
 echo "" > coverage.txt
 
-for d in $(go list ./... | grep -v github.com/prymitive/karma/internal/mapper); do
+for d in $(go list ./... | grep -vE 'prymitive/karma/internal/mapper/v017/(client|models)'); do
     go test \
         -coverprofile=profile.out \
-        -coverpkg=$(go list ./... | grep -v github.com/prymitive/karma/internal/mapper | tr '\n' ',') \
+        -coverpkg=$(go list ./... | grep -vE 'prymitive/karma/internal/mapper/v017/(client|models)' | tr '\n' ',') \
         $d 2>&1 | grep -v 'warning: no packages being tested depend on matches for pattern'
     if [ -f profile.out ]; then
         cat profile.out >> coverage.txt
