476a7add66a022982a418148faacc610155b7a94
Łukasz Mierzwa <l.mierzwa@gmail.com>
2020-04-12 17:32:13 +0100

    fix(ui): more margin between button on small screens

--- ui/src/Components/Grid/AlertGrid/AlertGroup/GroupHeader/GroupMenu.js
+++ ui/src/Components/Grid/AlertGrid/AlertGroup/GroupHeader/GroupMenu.js
@@ -145,7 +145,7 @@ const GroupMenu = observer(
                 onClick={this.collapse.toggle}
                 className={`${
                   themed ? "text-white" : "text-muted"
-                } cursor-pointer badge pl-0 components-label mr-0 components-grid-alertgroup-${
+                } cursor-pointer badge pl-0 pr-3 pr-sm-2 components-label mr-0 components-grid-alertgroup-${
                   group.id
                 }`}
                 data-toggle="dropdown"
