cd69c84a5f4d033cc65f33dbd135a410fd46bdbc
Łukasz Mierzwa <l.mierzwa@gmail.com>
2020-04-15 09:14:31 +0100

    fix(ci): correctly check for dry-run mode

--- scripts/prune-go-cache.sh
+++ scripts/prune-go-cache.sh
@@ -10,7 +10,7 @@ GOCACHE=$(go env GOCACHE)
 SIZE=`du -sxm ${GOCACHE} | awk '{print $1}'`
 echo "GOCACHE size: ${SIZE}MB"
 
-if [ "${DRY_RUN}" != "1" ] && [ "${TRAVIS_BRANCH}" == "master" ] && [ $SIZE -gt 3500 ]; then
+if [ "${DRY_RUN}" == "" ] && [ "${TRAVIS_BRANCH}" == "master" ] && [ $SIZE -gt 3500 ]; then
   echo "Pruning GOCACHE at ${GOCACHE}"
   find "${GOCACHE}" -type f -delete
 fi
