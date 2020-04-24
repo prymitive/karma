cdcc6981436057da9abe11ec79b1cc7b18061248
Łukasz Mierzwa <l.mierzwa@gmail.com>
2020-04-24 16:02:34 +0100

    fix(ui): rewrite UpgradeNeeded with hooks

--- ui/src/Components/Grid/UpgradeNeeded/index.js
+++ ui/src/Components/Grid/UpgradeNeeded/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import PropTypes from "prop-types";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -9,45 +9,31 @@ import { CenteredMessage } from "Components/CenteredMessage";
 
 import "csshake/scss/csshake-slow.scss";
 
-class UpgradeNeeded extends Component {
-  static propTypes = {
-    newVersion: PropTypes.string.isRequired,
-    reloadAfter: PropTypes.number.isRequired,
-  };
+const UpgradeNeeded = ({ newVersion, reloadAfter }) => {
+  useEffect(() => {
+    setTimeout(window.location.reload, reloadAfter);
+  }, [reloadAfter]);
 
-  reloadApp = () => {
-    window.location.reload();
-  };
-
-  componentDidMount() {
-    const { reloadAfter } = this.props;
-    this.timer = setTimeout(this.reloadApp, reloadAfter);
-  }
-
-  componentWillUnmount() {
-    clearTimeout(this.timer);
-    this.timer = null;
-  }
-
-  render() {
-    const { newVersion } = this.props;
-    return (
-      <CenteredMessage>
-        <div className="container-fluid text-center">
-          <div className="shake-slow shake-constant mb-4">
-            <FontAwesomeIcon
-              icon={faRocket}
-              className="screen-center-icon-big text-success"
-            />
-          </div>
-          <p className="lead text-muted">
-            <FontAwesomeIcon className="mr-2" icon={faSpinner} spin />
-            Upgrading to a new version: {newVersion}
-          </p>
+  return (
+    <CenteredMessage>
+      <div className="container-fluid text-center">
+        <div className="shake-slow shake-constant mb-4">
+          <FontAwesomeIcon
+            icon={faRocket}
+            className="screen-center-icon-big text-success"
+          />
         </div>
-      </CenteredMessage>
-    );
-  }
-}
+        <p className="lead text-muted">
+          <FontAwesomeIcon className="mr-2" icon={faSpinner} spin />
+          Upgrading to a new version: {newVersion}
+        </p>
+      </div>
+    </CenteredMessage>
+  );
+};
+UpgradeNeeded.propTypes = {
+  newVersion: PropTypes.string.isRequired,
+  reloadAfter: PropTypes.number.isRequired,
+};
 
 export { UpgradeNeeded };
