50f223642c1913f553aa8ba084275b7e9da6e9c8
Łukasz Mierzwa <l.mierzwa@gmail.com>
2020-04-26 20:53:36 +0100

    fix(ui): rewrite PayloadPreview with hooks

--- ui/src/Components/SilenceModal/PayloadPreview/index.js
+++ ui/src/Components/SilenceModal/PayloadPreview/index.js
@@ -1,32 +1,25 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
-import { observer } from "mobx-react";
+import { useObserver } from "mobx-react";
 
 import JSONPretty from "react-json-pretty";
 import * as theme from "react-json-pretty/dist/monikai";
 
 import { SilenceFormStore } from "Stores/SilenceFormStore";
 
-const PayloadPreview = observer(
-  class PayloadPreview extends Component {
-    static propTypes = {
-      silenceFormStore: PropTypes.instanceOf(SilenceFormStore).isRequired,
-    };
-
-    render() {
-      const { silenceFormStore } = this.props;
-
-      return (
-        <div className="mt-3">
-          <JSONPretty
-            json={silenceFormStore.data.toAlertmanagerPayload}
-            theme={theme}
-          />
-        </div>
-      );
-    }
-  }
-);
+const PayloadPreview = ({ silenceFormStore }) => {
+  return useObserver(() => (
+    <div className="mt-3">
+      <JSONPretty
+        json={silenceFormStore.data.toAlertmanagerPayload}
+        theme={theme}
+      />
+    </div>
+  ));
+};
+PayloadPreview.propTypes = {
+  silenceFormStore: PropTypes.instanceOf(SilenceFormStore).isRequired,
+};
 
 export { PayloadPreview };
