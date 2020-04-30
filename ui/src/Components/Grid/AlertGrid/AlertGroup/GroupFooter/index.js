2440b7778eb3630502048e947d8a8d383e66a209
Łukasz Mierzwa <l.mierzwa@gmail.com>
2020-04-30 17:27:45 +0100

    fix(ui): rewrite GroupHeader & GroupFooter components with hooks

--- ui/src/Components/Grid/AlertGrid/AlertGroup/GroupFooter/index.js
+++ ui/src/Components/Grid/AlertGrid/AlertGroup/GroupFooter/index.js
@@ -1,7 +1,7 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
-import { observer } from "mobx-react";
+import { useObserver } from "mobx-react";
 
 import { APIGroup } from "Models/API";
 import { StaticLabels } from "Common/Query";
@@ -11,92 +11,79 @@ import { FilteringLabel } from "Components/Labels/FilteringLabel";
 import { RenderNonLinkAnnotation, RenderLinkAnnotation } from "../Annotation";
 import { RenderSilence } from "../Silences";
 
-const GroupFooter = observer(
-  class GroupFooter extends Component {
-    static propTypes = {
-      group: APIGroup.isRequired,
-      alertmanagers: PropTypes.arrayOf(PropTypes.string).isRequired,
-      afterUpdate: PropTypes.func.isRequired,
-      alertStore: PropTypes.instanceOf(AlertStore).isRequired,
-      silenceFormStore: PropTypes.instanceOf(SilenceFormStore).isRequired,
-    };
-
-    render() {
-      const {
-        group,
-        alertmanagers,
-        afterUpdate,
-        alertStore,
-        silenceFormStore,
-      } = this.props;
-
-      return (
-        <div className="card-footer components-grid-alertgrid-alertgroup-footer px-2 py-1">
-          <div className="mb-1">
-            {group.shared.annotations
-              .filter((a) => a.isLink === false)
-              .map((a) => (
-                <RenderNonLinkAnnotation
-                  key={a.name}
-                  name={a.name}
-                  value={a.value}
-                  visible={a.visible}
-                  afterUpdate={afterUpdate}
-                  alertStore={alertStore}
-                />
-              ))}
-          </div>
-          {Object.entries(group.shared.labels).map(([name, value]) => (
-            <FilteringLabel
-              key={name}
-              name={name}
-              value={value}
+const GroupFooter = ({
+  group,
+  alertmanagers,
+  afterUpdate,
+  alertStore,
+  silenceFormStore,
+}) => {
+  return useObserver(() => (
+    <div className="card-footer components-grid-alertgrid-alertgroup-footer px-2 py-1">
+      <div className="mb-1">
+        {group.shared.annotations
+          .filter((a) => a.isLink === false)
+          .map((a) => (
+            <RenderNonLinkAnnotation
+              key={a.name}
+              name={a.name}
+              value={a.value}
+              visible={a.visible}
+              afterUpdate={afterUpdate}
               alertStore={alertStore}
             />
           ))}
-          {alertmanagers.map((am) => (
-            <FilteringLabel
-              key={am}
-              name={StaticLabels.AlertManager}
-              value={am}
-              alertStore={alertStore}
-            />
-          ))}
-          <FilteringLabel
-            name={StaticLabels.Receiver}
-            value={group.receiver}
-            alertStore={alertStore}
-          />
-          {group.shared.annotations
-            .filter((a) => a.isLink === true)
-            .map((a) => (
-              <RenderLinkAnnotation
-                key={a.name}
-                name={a.name}
-                value={a.value}
-              />
-            ))}
-          {Object.keys(group.shared.silences).length === 0 ? null : (
-            <div className="components-grid-alertgrid-alertgroup-shared-silence rounded-0 border-0">
-              {Object.entries(
-                group.shared.silences
-              ).map(([cluster, silences]) =>
-                silences.map((silenceID) =>
-                  RenderSilence(
-                    alertStore,
-                    silenceFormStore,
-                    afterUpdate,
-                    cluster,
-                    silenceID
-                  )
-                )
-              )}
-            </div>
+      </div>
+      {Object.entries(group.shared.labels).map(([name, value]) => (
+        <FilteringLabel
+          key={name}
+          name={name}
+          value={value}
+          alertStore={alertStore}
+        />
+      ))}
+      {alertmanagers.map((am) => (
+        <FilteringLabel
+          key={am}
+          name={StaticLabels.AlertManager}
+          value={am}
+          alertStore={alertStore}
+        />
+      ))}
+      <FilteringLabel
+        name={StaticLabels.Receiver}
+        value={group.receiver}
+        alertStore={alertStore}
+      />
+      {group.shared.annotations
+        .filter((a) => a.isLink === true)
+        .map((a) => (
+          <RenderLinkAnnotation key={a.name} name={a.name} value={a.value} />
+        ))}
+      {Object.keys(group.shared.silences).length === 0 ? null : (
+        <div className="components-grid-alertgrid-alertgroup-shared-silence rounded-0 border-0">
+          {Object.entries(group.shared.silences).map(([cluster, silences]) =>
+            silences.map((silenceID) =>
+              RenderSilence(
+                alertStore,
+                silenceFormStore,
+                afterUpdate,
+                cluster,
+                silenceID
+              )
+            )
           )}
         </div>
-      );
-    }
-  }
-);
+      )}
+    </div>
+  ));
+};
+GroupFooter.propTypes = {
+  group: APIGroup.isRequired,
+  alertmanagers: PropTypes.arrayOf(PropTypes.string).isRequired,
+  afterUpdate: PropTypes.func.isRequired,
+  alertStore: PropTypes.instanceOf(AlertStore).isRequired,
+  silenceFormStore: PropTypes.instanceOf(SilenceFormStore).isRequired,
+};
 
 export { GroupFooter };
