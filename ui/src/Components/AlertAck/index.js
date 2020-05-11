686f42af22cb51a74418fe49d2cffeaec1566e58
Łukasz Mierzwa <l.mierzwa@gmail.com>
2020-05-11 17:48:10 +0100

    fix(ui): rewrite AlertAck with hooks

--- ui/src/Components/AlertAck/index.js
+++ ui/src/Components/AlertAck/index.js
@@ -1,8 +1,8 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
-import { observable, action, computed, toJS } from "mobx";
-import { observer } from "mobx-react";
+import { toJS } from "mobx";
+import { useObserver, useLocalStore } from "mobx-react";
 
 import moment from "moment";
 
@@ -50,216 +50,174 @@ const newPendingSilence = (
   submitResult: null,
   isDone: false,
   isFailed: false,
-  fetch: null,
   error: null,
 });
 
-const AlertAck = observer(
-  class AlertAck extends Component {
-    static propTypes = {
-      alertStore: PropTypes.instanceOf(AlertStore).isRequired,
-      silenceFormStore: PropTypes.instanceOf(SilenceFormStore).isRequired,
-      group: APIGroup.isRequired,
-    };
-
-    constructor(props) {
-      super(props);
-      this.submitState = observable(
-        {
-          silencesByCluster: {},
-          reset() {
-            this.silencesByCluster = {};
-          },
-          pushSilence(cluster, silence) {
-            this.silencesByCluster[cluster] = silence;
-          },
-          markDone(cluster) {
-            this.silencesByCluster[cluster].isDone = true;
-          },
-          markFailed(cluster, err) {
-            this.silencesByCluster[cluster].isDone = true;
-            this.silencesByCluster[cluster].isFailed = true;
-            this.silencesByCluster[cluster].error = err;
-          },
-          get isIdle() {
-            return Object.keys(this.silencesByCluster).length === 0;
-          },
-          get isInprogress() {
-            return (
-              Object.values(this.silencesByCluster).filter(
-                (pendingSilence) => pendingSilence.isDone === false
-              ).length > 0
-            );
-          },
-          get isDone() {
-            return (
-              Object.values(this.silencesByCluster).filter(
-                (pendingSilence) => pendingSilence.isDone === true
-              ).length > 0
-            );
-          },
-          get isFailed() {
-            return (
-              Object.values(this.silencesByCluster).filter(
-                (pendingSilence) => pendingSilence.isFailed === true
-              ).length > 0
-            );
-          },
-          get errorMessages() {
-            return Object.values(this.silencesByCluster)
-              .filter((pendingSilence) => pendingSilence.error !== null)
-              .map((s) => s.error);
-          },
-        },
-        {
-          reset: action.bound,
-          pushSilence: action.bound,
-          markDone: action.bound,
-          markFailed: action.bound,
-          isIdle: computed,
-          isInprogress: computed,
-          isDone: computed,
-          isFailed: computed,
-          errorMessages: computed,
-        }
-      );
-    }
-
-    maybeTryAgainAfterError = (cluster, err) => {
-      if (this.submitState.silencesByCluster[cluster].membersToTry.length) {
-        this.handleAlertmanagerRequest(cluster);
-      } else {
-        this.submitState.markFailed(cluster, err);
-      }
-    };
-
-    handleAlertmanagerRequest = (cluster) => {
-      const { alertStore } = this.props;
-
-      const member = this.submitState.silencesByCluster[
-        cluster
-      ].membersToTry.pop();
-
-      const am = alertStore.data.getAlertmanagerByName(member);
-      if (am === undefined) {
-        const err = `Alertmanager instance "${member} not found`;
-        console.error(err);
-        this.maybeTryAgainAfterError(cluster, err);
-        return;
-      }
-
-      this.submitState.silencesByCluster[cluster].fetch = FetchPost(
-        `${am.uri}/api/v2/silences`,
-        {
-          body: JSON.stringify(
-            this.submitState.silencesByCluster[cluster].payload
-          ),
-          credentials: am.corsCredentials,
-          headers: {
-            "Content-Type": "application/json",
-            ...am.headers,
-          },
-        }
-      )
-        .then((result) => {
-          if (result.ok) {
-            return result
-              .json()
-              .then((r) => this.submitState.markDone(cluster));
-          } else {
-            result
-              .text()
-              .then((text) => this.maybeTryAgainAfterError(cluster, text));
-          }
-        })
-        .catch((err) => {
-          this.maybeTryAgainAfterError(cluster, err);
-        });
-    };
-
-    onACK = () => {
-      const { group, alertStore, silenceFormStore } = this.props;
-
-      if (this.submitState.isInprogress || this.submitState.isDone) {
-        return;
-      }
-
-      let author =
-        silenceFormStore.data.author !== ""
-          ? toJS(silenceFormStore.data.author)
-          : toJS(alertStore.settings.values.alertAcknowledgement.author);
-
-      if (alertStore.info.authentication.enabled) {
-        silenceFormStore.data.author = toJS(
-          alertStore.info.authentication.username
-        );
-        author = alertStore.info.authentication.username;
-      }
-
-      const alertmanagers = Object.entries(group.alertmanagerCount)
-        .filter(([amName, alertCount]) => alertCount > 0)
-        .map(([amName, _]) => amName);
-      const clusters = Object.entries(
-        alertStore.data.clustersWithoutReadOnly
-      ).filter(([clusterName, clusterMembers]) =>
-        alertmanagers.some((m) => clusterMembers.includes(m))
-      );
-
-      this.submitState.reset();
-      for (const [clusterName, clusterMembers] of clusters) {
-        const pendingSilence = newPendingSilence(
-          toJS(group),
-          toJS(clusterMembers),
-          toJS(alertStore.settings.values.alertAcknowledgement.durationSeconds),
-          author,
-          toJS(alertStore.settings.values.alertAcknowledgement.commentPrefix)
-        );
-        this.submitState.pushSilence(clusterName, pendingSilence);
-        this.handleAlertmanagerRequest(clusterName);
-      }
-    };
-
-    render() {
-      const { alertStore } = this.props;
-
-      if (alertStore.settings.values.alertAcknowledgement.enabled === false) {
-        return null;
-      }
-
-      const tooltipProps = {};
-      if (this.submitState.isFailed) {
-        tooltipProps.html = this.submitState.errorMessages[0];
-      } else {
-        tooltipProps.title =
-          "Acknowledge this alert with a short lived silence";
-      }
-
+const AlertAck = ({ alertStore, silenceFormStore, group }) => {
+  const submitState = useLocalStore(() => ({
+    silencesByCluster: {},
+    reset() {
+      this.silencesByCluster = {};
+    },
+    pushSilence(cluster, silence) {
+      this.silencesByCluster[cluster] = silence;
+    },
+    markDone(cluster) {
+      this.silencesByCluster[cluster].isDone = true;
+    },
+    markFailed(cluster, err) {
+      this.silencesByCluster[cluster].isDone = true;
+      this.silencesByCluster[cluster].isFailed = true;
+      this.silencesByCluster[cluster].error = err;
+    },
+    get isIdle() {
+      return Object.keys(this.silencesByCluster).length === 0;
+    },
+    get isInprogress() {
       return (
-        <TooltipWrapper {...tooltipProps}>
-          <span
-            className={`badge badge-pill components-label components-label-with-hover px-2 ${
-              this.submitState.isFailed
-                ? "badge-warning"
-                : this.submitState.isDone
-                ? "badge-success"
-                : "badge-secondary"
-            }`}
-            onClick={this.onACK}
-          >
-            {this.submitState.isIdle ? (
-              <FontAwesomeIcon icon={faCheck} fixedWidth />
-            ) : this.submitState.isInprogress ? (
-              <FontAwesomeIcon icon={faSpinner} fixedWidth spin />
-            ) : this.submitState.isFailed ? (
-              <FontAwesomeIcon icon={faExclamationCircle} fixedWidth />
-            ) : (
-              <FontAwesomeIcon icon={faCheckCircle} fixedWidth />
-            )}
-          </span>
-        </TooltipWrapper>
+        Object.values(this.silencesByCluster).filter(
+          (pendingSilence) => pendingSilence.isDone === false
+        ).length > 0
       );
+    },
+    get isDone() {
+      return (
+        Object.values(this.silencesByCluster).filter(
+          (pendingSilence) => pendingSilence.isDone === true
+        ).length > 0
+      );
+    },
+    get isFailed() {
+      return (
+        Object.values(this.silencesByCluster).filter(
+          (pendingSilence) => pendingSilence.isFailed === true
+        ).length > 0
+      );
+    },
+    get errorMessages() {
+      return Object.values(this.silencesByCluster)
+        .filter((pendingSilence) => pendingSilence.error !== null)
+        .map((s) => s.error);
+    },
+  }));
+
+  const maybeTryAgainAfterError = (cluster, err) => {
+    if (submitState.silencesByCluster[cluster].membersToTry.length) {
+      handleAlertmanagerRequest(cluster);
+    } else {
+      submitState.markFailed(cluster, err);
     }
-  }
-);
+  };
+
+  const handleAlertmanagerRequest = (cluster) => {
+    const member = submitState.silencesByCluster[cluster].membersToTry.pop();
+
+    const am = alertStore.data.getAlertmanagerByName(member);
+    if (am === undefined) {
+      const err = `Alertmanager instance "${member} not found`;
+      console.error(err);
+      maybeTryAgainAfterError(cluster, err);
+      return;
+    }
+
+    FetchPost(`${am.uri}/api/v2/silences`, {
+      body: JSON.stringify(submitState.silencesByCluster[cluster].payload),
+      credentials: am.corsCredentials,
+      headers: {
+        "Content-Type": "application/json",
+        ...am.headers,
+      },
+    })
+      .then((result) => {
+        if (result.ok) {
+          return result.json().then((r) => submitState.markDone(cluster));
+        } else {
+          result.text().then((text) => maybeTryAgainAfterError(cluster, text));
+        }
+      })
+      .catch((err) => {
+        maybeTryAgainAfterError(cluster, err);
+      });
+  };
+
+  const onACK = () => {
+    if (submitState.isInprogress || submitState.isDone) {
+      return;
+    }
+
+    let author =
+      silenceFormStore.data.author !== ""
+        ? toJS(silenceFormStore.data.author)
+        : toJS(alertStore.settings.values.alertAcknowledgement.author);
+
+    if (alertStore.info.authentication.enabled) {
+      silenceFormStore.data.author = toJS(
+        alertStore.info.authentication.username
+      );
+      author = alertStore.info.authentication.username;
+    }
+
+    const alertmanagers = Object.entries(group.alertmanagerCount)
+      .filter(([amName, alertCount]) => alertCount > 0)
+      .map(([amName, _]) => amName);
+    const clusters = Object.entries(
+      alertStore.data.clustersWithoutReadOnly
+    ).filter(([clusterName, clusterMembers]) =>
+      alertmanagers.some((m) => clusterMembers.includes(m))
+    );
+
+    submitState.reset();
+    for (const [clusterName, clusterMembers] of clusters) {
+      const pendingSilence = newPendingSilence(
+        toJS(group),
+        toJS(clusterMembers),
+        toJS(alertStore.settings.values.alertAcknowledgement.durationSeconds),
+        author,
+        toJS(alertStore.settings.values.alertAcknowledgement.commentPrefix)
+      );
+      submitState.pushSilence(clusterName, pendingSilence);
+      handleAlertmanagerRequest(clusterName);
+    }
+  };
+
+  return useObserver(() =>
+    alertStore.settings.values.alertAcknowledgement.enabled === false ? null : (
+      <TooltipWrapper
+        html={submitState.isFailed ? submitState.errorMessages[0] : null}
+        title={
+          submitState.isFailed
+            ? null
+            : "Acknowledge this alert with a short lived silence"
+        }
+      >
+        <span
+          className={`badge badge-pill components-label components-label-with-hover px-2 ${
+            submitState.isFailed
+              ? "badge-warning"
+              : submitState.isDone
+              ? "badge-success"
+              : "badge-secondary"
+          }`}
+          onClick={onACK}
+        >
+          {submitState.isIdle ? (
+            <FontAwesomeIcon icon={faCheck} fixedWidth />
+          ) : submitState.isInprogress ? (
+            <FontAwesomeIcon icon={faSpinner} fixedWidth spin />
+          ) : submitState.isFailed ? (
+            <FontAwesomeIcon icon={faExclamationCircle} fixedWidth />
+          ) : (
+            <FontAwesomeIcon icon={faCheckCircle} fixedWidth />
+          )}
+        </span>
+      </TooltipWrapper>
+    )
+  );
+};
+AlertAck.propTypes = {
+  alertStore: PropTypes.instanceOf(AlertStore).isRequired,
+  silenceFormStore: PropTypes.instanceOf(SilenceFormStore).isRequired,
+  group: APIGroup.isRequired,
+};
 
 export { AlertAck };
