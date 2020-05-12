c6e46a4c7ab5909b7bcde521549ad2b567cc205c
Łukasz Mierzwa <l.mierzwa@gmail.com>
2020-05-12 18:31:54 +0100

    feat(project): use github-release-notes to generate changelogs

--- tools.go
+++ tools.go
@@ -7,6 +7,7 @@
 package tools
 
 import (
+	_ "github.com/buchanae/github-release-notes"
 	_ "github.com/elazarl/go-bindata-assetfs"
 	_ "github.com/go-bindata/go-bindata/v3"
 	_ "github.com/golangci/golangci-lint/pkg/commands"
