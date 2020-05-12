b356eecbf3357bad3377627add797eccc1bd260d
Łukasz Mierzwa <l.mierzwa@gmail.com>
2020-05-12 23:20:13 +0100

    fix(ci): check if bootstrap and bootswatch versions are the same

--- make/lint-versions.mk
+++ make/lint-versions.mk
@@ -18,5 +18,14 @@ lint-nodejs-version:
 		exit 1; \
 	fi
 
+.PHONY: lint-bootstrap-version
+lint-bootstrap-version:
+	$(eval BOOTSTRAP_VERSION := $(shell grep bootstrap ui/package.json | cut -d: -f2 | tr -d ' ",'))
+	$(eval BOOTSWATCH_VERSION := $(shell grep bootswatch ui/package.json | cut -d: -f2 | tr -d ' ",'))
+	@if [ "$(BOOTSTRAP_VERSION)" != "$(BOOTSWATCH_VERSION)" ]; then \
+		echo "Bootstrap version mismatch: BOOTSTRAP_VERSION=$(BOOTSTRAP_VERSION) BOOTSWATCH_VERSION=$(BOOTSWATCH_VERSION)"; \
+		exit 1; \
+	fi
+
 .PHONY: lint-versions
 lint-versions: lint-golang-version lint-nodejs-version
