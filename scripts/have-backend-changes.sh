e1e21aa539975756e048396ed3d243035b383e1d
Łukasz Mierzwa <l.mierzwa@gmail.com>
2020-05-24 16:06:41 +0100

    fix(ci): run Go benchmarks only on Go code changes

---
+++ scripts/have-backend-changes.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+
+
+if [ "${TRAVIS_BRANCH}" == "master" ] && [ -n ${TRAVIS_COMMIT_RANGE} ]; then
+  RANGE="${TRAVIS_COMMIT_RANGE}"
+else
+  git fetch origin master
+  RANGE="FETCH_HEAD...${TRAVIS_COMMIT}"
+fi
+
+
+git log --no-merges --name-only --pretty=format: ${RANGE} | grep -Ev '^$' | sort | uniq | while read FILE ; do
+  if [[ "${FILE}" =~ ^cmd/.+ ]]; then
+    echo "[G] ${FILE}"
+    exit 1
+  elif [[ "${FILE}" =~ ^internal/.+ ]]; then
+    echo "[G] ${FILE}"
+    exit 1
+  else
+    echo "[ ] ${FILE}"
+  fi
+done
+
+exit 0
