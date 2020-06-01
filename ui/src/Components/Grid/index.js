f9e9d1f1f7adc7e7420d91a191d3ad2895c97edf
Łukasz Mierzwa <l.mierzwa@gmail.com>
2020-06-01 12:31:00 +0100

    fix(ui): refactor Grid component

--- ui/src/Components/Grid/index.js
+++ ui/src/Components/Grid/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import { observer } from "mobx-react";
+import { useObserver } from "mobx-react";
 
 import { AlertStore } from "Stores/AlertStore";
 import { Settings } from "Stores/Settings";
@@ -13,60 +13,43 @@ import { UpgradeNeeded } from "./UpgradeNeeded";
 import { ReloadNeeded } from "./ReloadNeeded";
 import { EmptyGrid } from "./EmptyGrid";
 
-const Grid = observer(({ alertStore, settingsStore, silenceFormStore }) => {
-  if (alertStore.info.upgradeNeeded) {
-    return (
+const Grid = ({ alertStore, settingsStore, silenceFormStore }) => {
+  return useObserver(() =>
+    alertStore.info.upgradeNeeded ? (
       <UpgradeNeeded newVersion={alertStore.info.version} reloadAfter={3000} />
-    );
-  }
-
-  if (alertStore.info.reloadNeeded) {
-    return <ReloadNeeded reloadAfter={4000} />;
-  }
-
-  if (alertStore.status.error) {
-    return <FatalError message={alertStore.status.error} />;
-  }
-
-  if (
-    alertStore.data.upstreams.counters &&
-    alertStore.data.upstreams.counters.total === 1 &&
-    alertStore.data.upstreams.counters.healthy === 0 &&
-    alertStore.data.upstreams.instances[0] &&
-    alertStore.data.upstreams.instances[0].error !== ""
-  ) {
-    return (
+    ) : alertStore.info.reloadNeeded ? (
+      <ReloadNeeded reloadAfter={4000} />
+    ) : alertStore.status.error ? (
+      <FatalError message={alertStore.status.error} />
+    ) : alertStore.data.upstreams.counters &&
+      alertStore.data.upstreams.counters.total === 1 &&
+      alertStore.data.upstreams.counters.healthy === 0 &&
+      alertStore.data.upstreams.instances[0] &&
+      alertStore.data.upstreams.instances[0].error !== "" ? (
       <FatalError message={alertStore.data.upstreams.instances[0].error} />
-    );
-  }
-
-  if (
-    alertStore.info.version !== "unknown" &&
-    alertStore.info.totalAlerts === 0
-  ) {
-    return <EmptyGrid />;
-  }
-
-  return (
-    <React.Fragment>
-      {alertStore.data.upstreams.instances
-        .filter((upstream) => upstream.error !== "")
-        .map((upstream) => (
-          <UpstreamError
-            key={upstream.name}
-            name={upstream.name}
-            message={upstream.error}
-          />
-        ))}
-
-      <AlertGrid
-        alertStore={alertStore}
-        settingsStore={settingsStore}
-        silenceFormStore={silenceFormStore}
-      />
-    </React.Fragment>
+    ) : alertStore.info.version !== "unknown" &&
+      alertStore.info.totalAlerts === 0 ? (
+      <EmptyGrid />
+    ) : (
+      <React.Fragment>
+        {alertStore.data.upstreams.instances
+          .filter((upstream) => upstream.error !== "")
+          .map((upstream) => (
+            <UpstreamError
+              key={upstream.name}
+              name={upstream.name}
+              message={upstream.error}
+            />
+          ))}
+        <AlertGrid
+          alertStore={alertStore}
+          settingsStore={settingsStore}
+          silenceFormStore={silenceFormStore}
+        />
+      </React.Fragment>
+    )
   );
-});
+};
 Grid.propTypes = {
   alertStore: PropTypes.instanceOf(AlertStore).isRequired,
   settingsStore: PropTypes.instanceOf(Settings).isRequired,
