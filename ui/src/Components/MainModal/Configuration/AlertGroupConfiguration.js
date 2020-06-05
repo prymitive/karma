dcd29ac83c8a58c86af9aab42a5e8eb8884c1fd0
Łukasz Mierzwa <l.mierzwa@gmail.com>
2020-06-05 15:38:59 +0100

    fix(ui): drop useLocalStore from Configuration

--- ui/src/Components/MainModal/Configuration/AlertGroupConfiguration.js
+++ ui/src/Components/MainModal/Configuration/AlertGroupConfiguration.js
@@ -1,38 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
-
-import { useObserver, useLocalStore } from "mobx-react";
 
 import InputRange from "react-input-range";
 
 import { Settings } from "Stores/Settings";
 
 const AlertGroupConfiguration = ({ settingsStore }) => {
-  const config = useLocalStore(() => ({
-    defaultRenderCount:
-      settingsStore.alertGroupConfig.config.defaultRenderCount,
-    setDefaultRenderCount(val) {
-      this.defaultRenderCount = val;
-    },
-  }));
+  const [defaultRenderCount, setDefaultRenderCount] = useState(
+    settingsStore.alertGroupConfig.config.defaultRenderCount
+  );
 
   const onChangeComplete = (value) => {
     settingsStore.alertGroupConfig.update({ defaultRenderCount: value });
   };
 
-  return useObserver(() => (
+  return (
     <div className="form-group mb-0 text-center">
       <InputRange
         minValue={1}
         maxValue={10}
         step={1}
-        value={config.defaultRenderCount}
+        value={defaultRenderCount}
         id="formControlRange"
-        onChange={config.setDefaultRenderCount}
+        onChange={setDefaultRenderCount}
         onChangeComplete={onChangeComplete}
       />
     </div>
-  ));
+  );
 };
 AlertGroupConfiguration.propTypes = {
   settingsStore: PropTypes.instanceOf(Settings).isRequired,
