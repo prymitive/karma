c15b3afb787af3a3c61912b668a1e2c7200e5f5f
Łukasz Mierzwa <l.mierzwa@gmail.com>
2020-06-05 15:38:59 +0100

    fix(ui): drop useLocalStore from Configuration

--- ui/src/Components/MainModal/Configuration/AlertGroupWidthConfiguration.js
+++ ui/src/Components/MainModal/Configuration/AlertGroupWidthConfiguration.js
@@ -1,7 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
-
-import { useObserver, useLocalStore } from "mobx-react";
 
 import debounce from "lodash.debounce";
 
@@ -10,30 +8,27 @@ import InputRange from "react-input-range";
 import { Settings } from "Stores/Settings";
 
 const AlertGroupWidthConfiguration = ({ settingsStore }) => {
-  const config = useLocalStore(() => ({
-    groupWidth: settingsStore.gridConfig.config.groupWidth,
-    setGroupWidth(val) {
-      this.groupWidth = val;
-    },
-  }));
+  const [groupWidth, setGroupWidth] = useState(
+    settingsStore.gridConfig.config.groupWidth
+  );
 
   const onChangeComplete = debounce((value) => {
     settingsStore.gridConfig.config.groupWidth = value;
   }, 200);
 
-  return useObserver(() => (
+  return (
     <div className="form-group mb-0 text-center">
       <InputRange
         minValue={300}
         maxValue={800}
         step={20}
-        value={config.groupWidth}
+        value={groupWidth}
         id="formControlRange"
-        onChange={config.setGroupWidth}
+        onChange={setGroupWidth}
         onChangeComplete={onChangeComplete}
       />
     </div>
-  ));
+  );
 };
 AlertGroupWidthConfiguration.propTypes = {
   settingsStore: PropTypes.instanceOf(Settings).isRequired,
--- ui/src/Components/MainModal/Configuration/FetchConfiguration.js
+++ ui/src/Components/MainModal/Configuration/FetchConfiguration.js
@@ -1,38 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
-
-import { useObserver, useLocalStore } from "mobx-react";
 
 import InputRange from "react-input-range";
 
 import { Settings } from "Stores/Settings";
 
 const FetchConfiguration = ({ settingsStore }) => {
-  const config = useLocalStore(() => ({
-    fetchInterval: settingsStore.fetchConfig.config.interval,
-    setFetchInterval(val) {
-      this.fetchInterval = val;
-    },
-  }));
+  const [fetchInterval, setFetchInterval] = useState(
+    settingsStore.fetchConfig.config.interval
+  );
 
   const onChangeComplete = (value) => {
     settingsStore.fetchConfig.setInterval(value);
   };
 
-  return useObserver(() => (
+  return (
     <div className="form-group mb-0 text-center">
       <InputRange
         minValue={10}
         maxValue={120}
         step={10}
-        value={config.fetchInterval}
+        value={fetchInterval}
         id="formControlRange"
         formatLabel={(value) => `${value}s`}
-        onChange={config.setFetchInterval}
+        onChange={setFetchInterval}
         onChangeComplete={onChangeComplete}
       />
     </div>
-  ));
+  );
 };
 FetchConfiguration.propTypes = {
   settingsStore: PropTypes.instanceOf(Settings).isRequired,
