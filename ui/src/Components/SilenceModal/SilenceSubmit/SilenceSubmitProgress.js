f063038c3086752b8cdc1c8986a33e58a13b6b87
Łukasz Mierzwa <l.mierzwa@gmail.com>
2020-06-06 19:54:33 +0100

    fix(ui): drop useLocalStore from SilenceSubmitProgress

--- ui/src/Components/SilenceModal/SilenceSubmit/SilenceSubmitProgress.js
+++ ui/src/Components/SilenceModal/SilenceSubmit/SilenceSubmitProgress.js
@@ -1,7 +1,5 @@
-import React, { useEffect, useCallback } from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
-
-import { useObserver, useLocalStore } from "mobx-react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleNotch } from "@fortawesome/free-solid-svg-icons/faCircleNotch";
@@ -10,139 +8,80 @@ import { faExclamationCircle } from "@fortawesome/free-solid-svg-icons/faExclama
 
 import { APISilenceMatcher } from "Models/API";
 import { AlertStore } from "Stores/AlertStore";
-import { FetchPost } from "Common/Fetch";
-
-const SubmitState = Object.freeze({
-  InProgress: "InProgress",
-  Done: "Done",
-  Failed: "Failed",
-});
-
-const SubmitIcon = ({ stateValue }) => {
-  return useObserver(() =>
-    stateValue === SubmitState.Done ? (
-      <FontAwesomeIcon icon={faCheckCircle} className="text-success" />
-    ) : stateValue === SubmitState.Failed ? (
-      <FontAwesomeIcon icon={faExclamationCircle} className="text-danger" />
-    ) : (
-      <FontAwesomeIcon icon={faCircleNotch} spin />
-    )
-  );
-};
-
-const SilenceLink = ({ uri, silenceID }) => (
-  <a
-    href={`${uri}/#/silences/${silenceID}`}
-    target="_blank"
-    rel="noopener noreferrer"
-  >
-    {silenceID}
-  </a>
-);
-SilenceLink.propTypes = {
-  uri: PropTypes.string.isRequired,
-  silenceID: PropTypes.string.isRequired,
-};
+import { useFetchAny } from "Hooks/useFetchAny";
 
 const SilenceSubmitProgress = ({ alertStore, cluster, members, payload }) => {
-  const submitState = useLocalStore(() => ({
-    membersToTry: [],
-    value: SubmitState.InProgress,
-    result: null,
-    markDone(result) {
-      this.result = result;
-      this.value = SubmitState.Done;
-    },
-    markFailed(result) {
-      this.result = result;
-      this.value = SubmitState.Failed;
-    },
-  }));
-
-  const handleAlertmanagerRequest = useCallback(() => {
-    const member = submitState.membersToTry.pop();
-
-    if (alertStore.data.isReadOnlyAlertmanager(member)) {
-      const err = `Alertmanager instance "${member}" is read-only`;
-      console.error(err);
-      if (submitState.membersToTry.length) {
-        return handleAlertmanagerRequest();
-      } else {
-        submitState.markFailed(err.message);
-      }
-      return;
-    }
-
-    const am = alertStore.data.getAlertmanagerByName(member);
-    if (am === undefined) {
-      const err = `Alertmanager instance "${member}" not found`;
-      console.error(err);
-      if (submitState.membersToTry.length) {
-        return handleAlertmanagerRequest();
-      } else {
-        submitState.markFailed(err.message);
-      }
-      return;
-    }
-
-    const parseOpenAPIResponse = (uri, response) => {
-      const link = <SilenceLink uri={uri} silenceID={response.silenceID} />;
-      submitState.markDone(link);
-      // return silenceID so we can assert it in tests
-      return response.silenceID;
-    };
-
-    FetchPost(`${am.uri}/api/v2/silences`, {
-      body: JSON.stringify(payload),
-      credentials: am.corsCredentials,
-      headers: {
-        "Content-Type": "application/json",
-        ...am.headers,
-      },
-    })
-      .then((result) => {
-        if (result.ok) {
-          return result
-            .json()
-            .then((r) => parseOpenAPIResponse(am.publicURI, r));
-        } else {
-          return result.text().then((text) => {
-            submitState.markFailed(text);
-            return text;
-          });
-        }
-      })
-      .catch((err) => {
-        if (submitState.membersToTry.length) {
-          return handleAlertmanagerRequest();
-        } else {
-          submitState.markFailed(err.message);
-        }
-      });
-  }, [alertStore.data, payload, submitState]);
+  const [upstreams, setUpstreams] = useState([]);
+  const { response, error, inProgress, responseURI } = useFetchAny(upstreams);
+  const [publicURIs, setPublicURIs] = useState({});
 
   useEffect(() => {
-    submitState.membersToTry = [...members];
-    handleAlertmanagerRequest();
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+    let uris = {};
+    let membersToTry = [];
+    for (const member of members) {
+      if (alertStore.data.isReadOnlyAlertmanager(member)) {
+        console.error(`Alertmanager instance "${member}" is read-only`);
+      } else {
+        const am = alertStore.data.getAlertmanagerByName(member);
+        if (am === undefined) {
+          console.error(`Alertmanager instance "${member}" not found`);
+        } else {
+          const uri = `${am.uri}/api/v2/silences`;
+          membersToTry.push({
+            uri: uri,
+            options: {
+              method: "POST",
+              body: JSON.stringify(payload),
+              credentials: am.corsCredentials,
+              headers: {
+                "Content-Type": "application/json",
+                ...am.headers,
+              },
+            },
+          });
+          uris[uri] = am.publicURI;
+        }
+      }
+    }
+    if (membersToTry.length) {
+      setPublicURIs(uris);
+      setUpstreams(membersToTry);
+    }
+  }, [alertStore.data, members, payload]);
 
-  return useObserver(() => (
+  return (
     <div className="d-flex mb-2">
       <div className="p-2 flex-fill my-auto flex-grow-0 flex-shrink-0">
-        <SubmitIcon stateValue={submitState.value} />
+        {inProgress ? (
+          <FontAwesomeIcon icon={faCircleNotch} spin />
+        ) : error ? (
+          <FontAwesomeIcon icon={faExclamationCircle} className="text-danger" />
+        ) : (
+          <FontAwesomeIcon icon={faCheckCircle} className="text-success" />
+        )}
       </div>
       <div className="p-2 mr-1 flex-fill my-auto flex-grow-0 flex-shrink-0">
         {cluster}
       </div>
       <div
         className={`p-2 flex-fill flex-grow-1 flex-shrink-1 rounded text-center ${
-          submitState.value === SubmitState.Failed ? "bg-light" : ""
+          error ? "bg-light" : ""
         }`}
       >
-        {submitState.result}
+        {error ? (
+          error
+        ) : response && responseURI ? (
+          <a
+            href={`${publicURIs[responseURI]}/#/silences/${response.silenceID}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {response.silenceID}
+          </a>
+        ) : null}
       </div>
     </div>
-  ));
+  );
 };
 SilenceSubmitProgress.propTypes = {
   cluster: PropTypes.string.isRequired,
