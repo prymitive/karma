09338a682a9403ac0ff987bbaa04782fb9d478be
Łukasz Mierzwa <l.mierzwa@gmail.com>
2020-06-18 13:43:34 +0100

    fix(tests): add more test coverage

--- internal/alertmanager/model_test.go
+++ internal/alertmanager/model_test.go
@@ -1,7 +1,11 @@
 package alertmanager
 
 import (
+	"fmt"
 	"testing"
+
+	"github.com/prymitive/karma/internal/config"
+	log "github.com/sirupsen/logrus"
 )
 
 type uriTest struct {
@@ -99,5 +103,93 @@ func TestAlertmanagerURI(t *testing.T) {
 			t.Errorf("[%d] Internal URI mismatch, expected '%s' => '%s', got '%s' (proxy: %v)",
 				i, test.rawURI, test.internalURI, am.InternalURI(), test.proxy)
 		}
+	}
+}
+
+func TestAlertmanagerSilenceByID(t *testing.T) {
+	am, err := NewAlertmanager("cluster", "test", "http://localhost")
+	if err != nil {
+		t.Error(err)
+	}
+
+	_, err = am.SilenceByID("foo")
+	if err == nil {
+		t.Error("am.SilenceByID(foo) didn't return any error")
+	}
+}
+
+func TestAlertmanagerInternalURI(t *testing.T) {
+	type testCaseT struct {
+		prefix string
+		proxy  bool
+		uri    string
+	}
+	tests := []testCaseT{
+		{
+			prefix: "/",
+			proxy:  false,
+			uri:    "http://localhost",
+		},
+		{
+			prefix: "/",
+			proxy:  true,
+			uri:    "/proxy/alertmanager/default",
+		},
+		{
+			prefix: "/root",
+			proxy:  true,
+			uri:    "/root/proxy/alertmanager/default",
+		},
+		{
+			prefix: "/root/",
+			proxy:  true,
+			uri:    "/root/proxy/alertmanager/default",
+		},
+		{
+			prefix: "root",
+			proxy:  true,
+			uri:    "/root/proxy/alertmanager/default",
+		},
+		{
+			prefix: "root/",
+			proxy:  true,
+			uri:    "/root/proxy/alertmanager/default",
+		},
+	}
+
+	for _, testCase := range tests {
+		t.Run(fmt.Sprintf("prefix=%q proxy=%v uri=%q", testCase.prefix, testCase.proxy, testCase.uri), func(t *testing.T) {
+			config.Config.Listen.Prefix = testCase.prefix
+			am, err := NewAlertmanager("cluster", "default", "http://localhost", WithProxy(testCase.proxy))
+			if err != nil {
+				t.Error(err)
+			}
+
+			uri := am.InternalURI()
+			if uri != testCase.uri {
+				t.Errorf("am.InternalURI() returned %q, expected %q", uri, testCase.uri)
+			}
+		})
+	}
+}
+
+func TestAlertmanagerSanitizedURI(t *testing.T) {
+	am, err := NewAlertmanager("cluster", "test", "http://user:pass@localhost")
+	if err != nil {
+		t.Error(err)
+	}
+
+	uri := am.SanitizedURI()
+	if uri != "http://user:xxx@localhost" {
+		t.Errorf("am.SanitizedURI(http://user:pass@localhost) returned %q", uri)
+	}
+}
+
+func TestAlertmanagerPullWithInvalidURI(t *testing.T) {
+	log.SetLevel(log.PanicLevel)
+	am, _ := NewAlertmanager("cluster", "test", "%gh&%ij")
+	err := am.Pull()
+	if err == nil {
+		t.Error("am.Pull(invalid uri) didn't return any error")
 	}
 }
