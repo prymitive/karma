51ac13db31f0517e120e549d4c0d6a0973e0dbb8
Łukasz Mierzwa <l.mierzwa@gmail.com>
2020-07-19 13:37:00 +0100

    fix(ui): don't render silence fallback on alert group unmount

---
+++ ui/src/Components/Grid/AlertGrid/AlertGroup/Silences.test.js
@@ -0,0 +1,109 @@
+import React from "react";
+
+import { mount } from "enzyme";
+
+import toDiffableHtml from "diffable-html";
+
+import { MockSilence } from "__mocks__/Alerts";
+import { AlertStore } from "Stores/AlertStore";
+import { SilenceFormStore } from "Stores/SilenceFormStore";
+import { RenderSilence } from "./Silences";
+
+describe("<RenderSilence />", () => {
+  let alertStore;
+  let silenceFormStore;
+
+  beforeEach(() => {
+    alertStore = new AlertStore([]);
+    silenceFormStore = new SilenceFormStore();
+  });
+
+  it("renders fallback text if silence is not present in AlertStore", () => {
+    const tree = mount(
+      <RenderSilence
+        alertStore={alertStore}
+        silenceFormStore={silenceFormStore}
+        afterUpdate={jest.fn()}
+        cluster="fakeCluster"
+        silenceID="1234567890"
+      />
+    );
+    expect(tree.text()).toBe("Silenced by 1234567890");
+    expect(toDiffableHtml(tree.html())).toMatchSnapshot();
+  });
+
+  it("renders ManagedSilence if silence is present in AlertStore", () => {
+    const silence = MockSilence();
+
+    alertStore.data.silences = { fakeCluster: { [silence.id]: silence } };
+
+    const tree = mount(
+      <RenderSilence
+        alertStore={alertStore}
+        silenceFormStore={silenceFormStore}
+        afterUpdate={jest.fn()}
+        cluster="fakeCluster"
+        silenceID={silence.id}
+      />
+    );
+    expect(tree.find("ManagedSilence")).toHaveLength(1);
+    expect(tree.text()).toMatch(/Mocked Silence/);
+    expect(toDiffableHtml(tree.html())).toMatchSnapshot();
+  });
+
+  it("re-render when silence was removed AlertStore is a no-op", () => {
+    const silence = MockSilence();
+
+    alertStore.data.silences = { fakeCluster: { [silence.id]: silence } };
+
+    const tree = mount(
+      <RenderSilence
+        alertStore={alertStore}
+        silenceFormStore={silenceFormStore}
+        afterUpdate={jest.fn()}
+        cluster="fakeCluster"
+        silenceID={silence.id}
+      />
+    );
+    expect(tree.find("ManagedSilence")).toHaveLength(1);
+    const snapshot = toDiffableHtml(tree.html());
+
+    alertStore.data.silences = {};
+
+    tree.setProps({});
+    expect(tree.find("ManagedSilence")).toHaveLength(1);
+    expect(toDiffableHtml(tree.html())).toBe(snapshot);
+  });
+
+  it("re-render when silence ID was changed updates it", () => {
+    const tree = mount(
+      <RenderSilence
+        alertStore={alertStore}
+        silenceFormStore={silenceFormStore}
+        afterUpdate={jest.fn()}
+        cluster="fakeCluster"
+        silenceID="silence1"
+      />
+    );
+    expect(tree.text()).toBe("Silenced by silence1");
+
+    tree.setProps({ silenceID: "silence2" });
+    expect(tree.text()).toBe("Silenced by silence2");
+  });
+
+  it("re-render when cluster name was changed updates it", () => {
+    const tree = mount(
+      <RenderSilence
+        alertStore={alertStore}
+        silenceFormStore={silenceFormStore}
+        afterUpdate={jest.fn()}
+        cluster="cluster1"
+        silenceID="1234567890"
+      />
+    );
+    expect(tree.text()).toBe("Silenced by 1234567890");
+
+    tree.setProps({ cluster: "cluster2" });
+    expect(tree.text()).toBe("Silenced by 1234567890");
+  });
+});
