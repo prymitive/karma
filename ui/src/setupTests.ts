62336c77aa77b147acf1f526a902afb8644ed825
Łukasz Mierzwa <l.mierzwa@gmail.com>
2020-07-23 12:57:38 +0100

    fix(ui): migrate setupTests to typescript

--- ui/src/setupTests.ts
+++ ui/src/setupTests.ts
@@ -15,16 +15,19 @@ require("jest-canvas-mock");
 // used to mock current time since we render moment.fromNow() in some places
 require("jest-date-mock");
 
-// ensure that all console messages throw errors
-for (const level of ["error", "warn", "info", "log", "trace"]) {
-  // https://reactjs.org/blog/2019/08/08/react-v16.9.0.html#new-deprecations
-  const reactDeprecationWarning = /.*has been renamed, and is not recommended for use.*/;
-  global.console[level] = (message, ...args) => {
-    if (reactDeprecationWarning.test(message) === false) {
-      throw new Error(`message=${message} args=${args}`);
-    }
-  };
-}
+// https://reactjs.org/blog/2019/08/08/react-v16.9.0.html#new-deprecations
+const reactDeprecationWarning = /.*has been renamed, and is not recommended for use.*/;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const consoleHandler = (message?: any, ...args: any): void => {
+  if (reactDeprecationWarning.test(message as string) === false) {
+    throw new Error(`message=${message} args=${args}`);
+  }
+};
+global.console.error = consoleHandler;
+global.console.warn = consoleHandler;
+global.console.info = consoleHandler;
+global.console.log = consoleHandler;
+global.console.trace = consoleHandler;
 
 FetchRetryConfig.minTimeout = 2;
 FetchRetryConfig.maxTimeout = 10;
