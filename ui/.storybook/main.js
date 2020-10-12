f57cf94849c837272032a6a8a084059029a083bf
Łukasz Mierzwa <l.mierzwa@gmail.com>
2020-10-12 11:25:16 +0100

    fix(ci): migrate storybook config to new format

---
+++ ui/.storybook/main.js
@@ -0,0 +1,4 @@
+module.exports = {
+  addons: ["@storybook/preset-create-react-app"],
+  stories: ["../src/**/*.stories.tsx"],
+};
--- ui/.storybook/presets.js
+++
@@ -1,1 +0,0 @@
-module.exports = ["@storybook/preset-create-react-app"];
