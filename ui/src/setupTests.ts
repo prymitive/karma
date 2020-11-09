903e7ed21242b616b03a35ce9284cc8ffe040607
Łukasz Mierzwa <l.mierzwa@gmail.com>
2020-11-09 16:45:36 +0000

    fix(tests): use enzyme adapter for react v17

--- ui/src/setupTests.ts
+++ ui/src/setupTests.ts
@@ -1,7 +1,7 @@
 import React from "react";
 
 import Enzyme from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 
 import { useInView } from "react-intersection-observer";
 
