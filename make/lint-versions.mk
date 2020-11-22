ae32911f5d2d3519fb761a0b5f7271db81c448cd
Łukasz Mierzwa <l.mierzwa@gmail.com>
2020-11-22 12:21:00 +0000

    chore(actions): use actions to sync node & go version

--- make/lint-versions.mk
+++ make/lint-versions.mk
@@ -1,25 +1,3 @@
-.PHONY: find-golang-versions
-find-golang-versions:
-	@find $(CURDIR) -name Dockerfile -exec grep 'FROM golang' {} \; | cut -d: -f2 | cut -d'-' -f1
-	@grep go-version $(CURDIR)/.github/workflows/* | awk '{print $$3}'
-
-.PHONY: lint-golang-version
-lint-golang-version:
-	$(eval VERSIONS := $(shell make find-golang-versions | sort | uniq))
-	$(eval COUNT := $(shell echo "$(VERSIONS)" | wc -w))
-	@if [ $(COUNT) -gt 1 ]; then echo "Multiple Go versions: $(VERSIONS)" ; exit 1 ; fi
-
-.PHONY: find-nodejs-versions
-find-nodejs-versions:
-	@find $(CURDIR) -name Dockerfile -exec grep 'FROM node' {} \; | cut -d: -f2 | cut -d'-' -f1
-	@grep node-version $(CURDIR)/.github/workflows/* | awk '{print $$3}'
-
-.PHONY: lint-nodejs-version
-lint-nodejs-version:
-	$(eval VERSIONS := $(shell make find-nodejs-versions | sort | uniq))
-	$(eval COUNT := $(shell echo "$(VERSIONS)" | wc -w))
-	@if [ $(COUNT) -gt 1 ]; then echo "Multiple NodeJS versions: $(VERSIONS)" ; exit 1 ; fi
-
 .PHONY: lint-bootstrap-version
 lint-bootstrap-version:
 	$(eval BOOTSTRAP_VERSION := $(shell grep bootstrap ui/package.json | cut -d: -f2 | tr -d ' ",'))
