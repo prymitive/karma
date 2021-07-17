5dd75b403593bfb912d0f3e2f49d7c1055d5e033
Łukasz Mierzwa <l.mierzwa@gmail.com>
2021-07-17 14:44:36 +0100

    fix(ci): rename commitlint config file

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -7,7 +7,7 @@ module.exports = {
     "subject-case": [
       2,
       "never",
-      ["sentence-case", "start-case", "pascal-case", "upper-case"]
+      ["sentence-case", "start-case", "pascal-case", "upper-case"],
     ],
     "subject-empty": [2, "never"],
     "subject-full-stop": [2, "never", "."],
@@ -27,8 +27,8 @@ module.exports = {
         "refactor",
         "revert",
         "style",
-        "test"
-      ]
-    ]
-  }
+        "test",
+      ],
+    ],
+  },
 };
