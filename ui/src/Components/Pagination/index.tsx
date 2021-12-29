9171a529ca41574ace2335b5f9e557ff6793c921
Łukasz Mierzwa <l.mierzwa@gmail.com>
2021-12-29 11:14:31 +0000

    fix(ui): use a custom pagination component

--- ui/src/Components/Pagination/index.tsx
+++ ui/src/Components/Pagination/index.tsx
@@ -2,8 +2,6 @@ import { useState, useEffect, FC } from "react";
 
 import { useHotkeys } from "react-hotkeys-hook";
 
-import Pagination from "react-js-pagination";
-
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleLeft } from "@fortawesome/free-solid-svg-icons/faAngleLeft";
 import { faAngleRight } from "@fortawesome/free-solid-svg-icons/faAngleRight";
@@ -27,6 +25,7 @@ const PageSelect: FC<{
   initialPage = 1,
   setPageCallback,
 }) => {
+  const [pages, setPages] = useState<number[]>([]);
   const [activePage, setActivePage] = useState<number>(initialPage);
 
   const onChange = (page: number) => {
@@ -35,12 +34,29 @@ const PageSelect: FC<{
   };
 
   useEffect(() => {
+    const allPages = Array.from(Array(totalPages).keys()).map((k) => k + 1);
+    const gap = IsMobile() ? 1 : 2;
+
+    let minPage = activePage - gap;
+    let maxPage = activePage + gap;
+    if (minPage < 1) {
+      maxPage = Math.min(maxPage + -minPage + 1, totalPages);
+      minPage = 1;
+    }
+    if (maxPage > totalPages) {
+      minPage = Math.max(1, minPage - (maxPage - totalPages));
+      maxPage = totalPages;
+    }
+    setPages(allPages.slice(minPage - 1, maxPage));
+  }, [activePage, totalPages]);
+
+  useEffect(() => {
     if (activePage > totalPages) {
       const page = Math.max(1, totalPages);
       setActivePage(page);
       setPageCallback(page);
     }
-  }, [activePage, maxPerPage, totalPages, setPageCallback]);
+  }, [activePage, totalPages, setPageCallback]);
 
   useHotkeys(
     "left",
@@ -67,25 +83,65 @@ const PageSelect: FC<{
   return (
     <div className="components-pagination">
       {totalItemsCount > maxPerPage ? (
-        <div className="mt-3">
-          <Pagination
+        <ul className="pagination justify-content-center mt-3">
+          {totalPages >= 10 ? (
+            <li className={`page-item ${activePage > 1 ? "" : "disabled"}`}>
+              <button className="page-link" onClick={() => onChange(1)}>
+                <FontAwesomeIcon icon={faAngleDoubleLeft} />
+              </button>
+            </li>
+          ) : null}
+          <li className={`page-item ${activePage > 1 ? "" : "disabled"}`}>
+            <button
+              className="page-link"
+              onClick={() => onChange(Math.max(1, activePage - 1))}
+            >
+              <FontAwesomeIcon icon={faAngleLeft} />
+            </button>
+          </li>
+          {pages.map((page) => (
+            <li
+              key={page}
+              className={`page-item ${
+                page === activePage ? "active font-weight-bold" : ""
+              }`}
+            >
+              <button className="page-link" onClick={() => onChange(page)}>
+                {page}
+              </button>
+            </li>
+          ))}
+          <li
+            className={`page-item ${activePage < totalPages ? "" : "disabled"}`}
+          >
+            <button
+              className="page-link"
+              onClick={() => onChange(Math.min(activePage + 1, totalPages))}
+            >
+              <FontAwesomeIcon icon={faAngleRight} />
+            </button>
+          </li>
+          {totalPages >= 10 ? (
+            <li
+              className={`page-item ${
+                activePage < totalPages ? "" : "disabled"
+              }`}
+            >
+              <button
+                className="page-link"
+                onClick={() => onChange(totalPages)}
+              >
+                <FontAwesomeIcon icon={faAngleDoubleRight} />
+              </button>
+            </li>
+          ) : null}
+          {/*           <Pagination
             activePage={activePage}
             itemsCountPerPage={maxPerPage}
             totalItemsCount={totalItemsCount}
             pageRangeDisplayed={IsMobile() ? 3 : 5}
-            onChange={onChange}
-            hideFirstLastPages={totalPages < 10}
-            innerClass="pagination justify-content-center"
-            itemClass="page-item"
-            linkClass="page-link"
-            activeClass="active"
-            activeLinkClass="font-weight-bold"
-            prevPageText={<FontAwesomeIcon icon={faAngleLeft} />}
-            nextPageText={<FontAwesomeIcon icon={faAngleRight} />}
-            firstPageText={<FontAwesomeIcon icon={faAngleDoubleLeft} />}
-            lastPageText={<FontAwesomeIcon icon={faAngleDoubleRight} />}
-          />
-        </div>
+          /> */}
+        </ul>
       ) : null}
     </div>
   );
