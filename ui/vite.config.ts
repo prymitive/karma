dc4e936a53e24977cbcf2ee703e0e322a2a6d862
Łukasz Mierzwa <l.mierzwa@gmail.com>
2023-12-01 10:31:07 +0000

    fix(ui): add source maps

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -4,10 +4,12 @@ import viteTsconfigPaths from "vite-tsconfig-paths";
 import legacy from "@vitejs/plugin-legacy";
 
 export default defineConfig({
-  base: "",
   plugins: [react(), viteTsconfigPaths(), legacy()],
   server: {
     open: true,
     port: 3000,
+  },
+  build: {
+    sourcemap: true,
   },
 });
