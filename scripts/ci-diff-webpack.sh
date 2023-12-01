70e5b23d823f311a7cb40ecdf306d7668147fb10
Łukasz Mierzwa <l.mierzwa@gmail.com>
2023-12-01 10:06:31 +0000

    fix(ci): fix paths

--- scripts/ci-diff-webpack.sh
+++ scripts/ci-diff-webpack.sh
@@ -6,12 +6,12 @@ set -o pipefail
 git fetch origin main
 git reset --hard FETCH_HEAD
 
-make -C ui build/stats.json
+make -C ui dist/stats.json
 mv ui/dist/stats.json main.json
 
 make clean
 git reset --hard ${GITHUB_SHA}
-make -C ui build/stats.json
+make -C ui dist/stats.json
 
 ./scripts/cra-bundle-stats-diff.py main.json ui/dist/stats.json | tee diff.html
 ./scripts/pr-comment.py "Webpack bundle size diff" diff.html html
