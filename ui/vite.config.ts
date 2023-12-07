218ff2795486dcce9eae860f96d8294a20adf8e6
Łukasz Mierzwa <l.mierzwa@gmail.com>
2023-12-07 09:53:43 +0000

    fix(ui): use relative paths for assets
    Fixes #5623.

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -4,6 +4,7 @@ import viteTsconfigPaths from "vite-tsconfig-paths";
 import legacy from "@vitejs/plugin-legacy";
 
 export default defineConfig({
+  base: "./",
   plugins: [react(), viteTsconfigPaths(), legacy()],
   server: {
     open: true,
