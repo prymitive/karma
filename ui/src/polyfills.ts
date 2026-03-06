bcbc31a7c1f9913d86f9a50bc568811cb2f11957
Lukasz Mierzwa <l.mierzwa@gmail.com>
2026-03-06 15:50:16 +0000

    fix(ui): remove unused polyfills

--- ui/src/polyfills.ts
+++ ui/src/polyfills.ts
@@ -1,11 +1,3 @@
-// https://github.com/facebook/create-react-app/blob/master/packages/react-app-polyfill/README.md
-// IE is not supported (lacks Proxy) but that pollyfill provides fetch and other needed features
-import "react-app-polyfill/ie11";
-import "react-app-polyfill/stable";
-
-// https://www.npmjs.com/package/react-intersection-observer#polyfill
-import "intersection-observer";
-
 declare global {
   interface Window {
     ResizeObserver: () => void;
